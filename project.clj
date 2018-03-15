8a97e463f93dcadae3c854e025bf76a0effc07d4
Lee Spector <lspector@hampshire.edu>
2018-03-14 22:44:49 -0400

    Update version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject propel "0.1.0"
-  :description "Tom Helmuth's small Push implementation in Clojure."
+(defproject propel "0.2.0"
+  :description "Lee Spector's Plusification of Tom Helmuth's small Push implementation in Clojure."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
